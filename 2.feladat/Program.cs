50aa77dd1c5f8f7bd69d253d2b6b1c281d6ef226
haricles <erik00000@hotmail.com>
2024-01-23 20:26:01 +0100

    felhasználótól bekérünk számot és kör kerület és területet számolok

---
+++ 2.feladat/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Készíts egy rövid programot, amely a felhasználótól bekéri a kör sugarát, és ennek alapján kiszámolja a kör kerületét és területét!
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gyakorlas
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Add meg a kör sugarát: ");
+            int r = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"A kör kerülete: {2 * Math.PI * r} és a kört területe: {Math.Pow(r, 2) * Math.PI}");
+            Console.ReadKey();
+        }
+    }
+}
