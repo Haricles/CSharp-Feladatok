08df28df68d93bb766502efb95e52f83da6c9ea5
haricles <erik00000@hotmail.com>
2024-01-23 20:18:12 +0100

    kör területe

--- 1.feladat/1.feladat/1.feladat/Program.cs
+++ 1.feladat/1.feladat/1.feladat/Program.cs
@@ -1,7 +1,9 @@
 ﻿/* 
 Készíts egy rövid programot, amely
-tartalmaz egy- és többsoros kommentet,
-és kiír valamilyen szöveget a képernyőre!
+kommentként tartalmazza a program funkciójának leírását,
+konstansként tárolja PI értékét,
+egy másik változóban tárolja egy kör sugarának nagyságát (cm-ben megadva),
+kiszámolja és a képernyőre kiírja a kör kerületét és területét!
 */
 
 
@@ -18,10 +20,11 @@ namespace _1.feladat
     {
         static void Main()
         {
-            /* komment
-             komment */
-            Console.WriteLine("Helló Világ!");
+            double a = Math.PI;
+            int sugar = 3;
+            Console.WriteLine($" A kört területe : {Math.Pow(a,2) * sugar}");
             Console.ReadKey();
+
         }
     }
 }
