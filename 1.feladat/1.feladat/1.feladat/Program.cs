0a86d9166cc0bca551321f25c03a432b67e582d7
haricles <erik00000@hotmail.com>
2024-01-23 20:08:28 +0100

    több soros komment + szöveg kiírás

---
+++ 1.feladat/1.feladat/1.feladat/Program.cs
@@ -0,0 +1,27 @@
+﻿/* 
+Készíts egy rövid programot, amely
+tartalmaz egy- és többsoros kommentet,
+és kiír valamilyen szöveget a képernyőre!
+*/
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.feladat
+{
+    class Program
+    {
+        static void Main()
+        {
+            /* komment
+             komment */
+            Console.WriteLine("Helló Világ!");
+            Console.ReadKey();
+        }
+    }
+}
