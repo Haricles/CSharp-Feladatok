727219fae76887001b19b55a493c293b9393d064
haricles <erik00000@hotmail.com>
2024-01-28 19:17:58 +0100

    Páros vagy páratlan

---
+++ 6.feladat/Program.cs
@@ -0,0 +1,30 @@
+﻿/* 
+Készíts egy programot, ami bekér egy számot a felhasználótól, majd kiírja, hogy a megadott szám páros-e vagy páratlan!
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.feladat
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Kérem adjon meg egy szamot: ");
+            int szam = Convert.ToInt32(Console.ReadLine());
+            if (szam % 2 == 0)
+            {
+                Console.WriteLine("Ez a szám páros!");
+            }
+            else
+            {
+                Console.WriteLine("Ez a szám páratlan!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
