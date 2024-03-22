f51b61eb5ffd33b883f229fce7e33254d1cd5de3
haricles <erik00000@hotmail.com>
2024-03-22 16:08:48 +0100

    0-10-ig kiírja a páros számokat

---
+++ 13.feladat/Program.cs
@@ -0,0 +1,29 @@
+﻿/* 
+Írj egy programot, amely kiírja a páros számokat 1 és 10 között!
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gyakorlas
+{
+    class Program
+    {
+        static void Main()
+        {
+            int i = 0;
+            while (i < 10)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                i += 1;
+            }
+            Console.ReadKey();
+        }
+    }
+}
