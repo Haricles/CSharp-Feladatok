4941e653af4bb74a9cf59240eb1463df02802d7f
haricles <erik00000@hotmail.com>
2024-03-22 16:23:05 +0100

    10-től 0-ig páratlan számok kiírása

---
+++ 16.feladat/Program.cs
@@ -0,0 +1,27 @@
+﻿/* 
+Írj egy programot, amely kiírja a páratlan számokat csökkenő sorrendben 1 és 10 között!
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gyakorlas
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int i =10; i > 0; i--)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
