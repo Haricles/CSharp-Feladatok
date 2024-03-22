50514c0bbd3c6f2e28093cc8547c832ff31db26c
haricles <erik00000@hotmail.com>
2024-03-22 16:16:11 +0100

    paros szamok 0-10 között for loopal

---
+++ 14.feladat/Program.cs
@@ -0,0 +1,27 @@
+﻿/* 
+Írj egy programot, amely kiírja a páros számokat 1 és 10 között!
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gyakorlas
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int i =0; i < 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
