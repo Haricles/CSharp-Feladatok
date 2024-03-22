ebdd73b61becf59da9fe3965638686631fa06113
haricles <erik00000@hotmail.com>
2024-03-22 16:18:34 +0100

    szamok kiirasa 10-től 0-ig

---
+++ 15.feladat/Program.cs
@@ -0,0 +1,24 @@
+﻿/* 
+Írj egy programot, amely csökkenő sorrendben írja ki a számokat 1 és 10 között!
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gyakorlas
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int i =10; i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
