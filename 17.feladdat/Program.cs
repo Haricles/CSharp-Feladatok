1d8058f365000d427787e2f9c01edc16ea880e21
haricles <erik00000@hotmail.com>
2024-03-23 09:38:51 +0100

    a bekert szöveget annyiszor írja ki amennyiszer a felhasználó szeretné

---
+++ 17.feladdat/Program.cs
@@ -0,0 +1,26 @@
+﻿/* 
+Írj egy programot, amely a felhasználó által meghatározott alkalommal írja ki a bekért szöveget!
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gyakorlas
+{
+    class Program
+    {
+        static void Main()
+        {
+            int szam = Convert.ToInt32(Console.ReadLine());
+            string szoveg = Console.ReadLine();
+            for (int i = 0; i < szam; i++)
+            {
+                Console.WriteLine(szoveg);
+            }
+            Console.ReadKey();
+        }
+    }
+}
